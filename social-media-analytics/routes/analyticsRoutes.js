11ab4fdbfb2f572c9a2cdd167e2aed5d107e5a7a
Shivam Kumar Pathak <114474974+Shivampathak-1@users.noreply.github.com>
2025-04-04 11:49:42 +0530

    post-type-removed

--- social-media-analytics/routes/analyticsRoutes.js
+++ social-media-analytics/routes/analyticsRoutes.js
@@ -44,23 +44,6 @@ router.get('/posts', async (req, res) => {
             const userPosts = await fetchPosts(userId, access_token);
             posts.push(...userPosts);
         }));
-
-        if (type === 'latest') {
-            const latestPosts = sortPostsByTimestamp(posts);
-            res.json({ latestPosts });
-        } else if (type === 'popular') {
-            const postCommentCounts = {};
-
-            await Promise.all(posts.map(async (post) => {
-                const comments = await fetchComments(post.id, access_token);
-                postCommentCounts[post.id] = comments.length;
-            }));
-
-            const popularPosts = sortPostsByComments(postCommentCounts);
-            res.json({ popularPosts });
-        } else {
-            res.status(400).json({ message: 'Invalid type parameter' });
-        }
     } catch (error) {
         console.error('Error in /posts route:', error.message);
         res.status(500).json({ message: 'Error fetching posts' });
